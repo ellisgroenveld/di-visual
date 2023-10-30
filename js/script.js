319cd2a5bda994b248a544ee777625be3b23a222
nathangroenveld <2062294groenveld@zuyd.nl>
2023-10-30 13:24:38 +0100

    d3

--- js/script.js
+++ js/script.js
@@ -1,79 +1,89 @@
-const circle1 = document.getElementById('circle1');
-const circle2 = document.getElementById('circle2');
-const circle3 = document.getElementById('circle3');
-const circle4 = document.getElementById('circle4');
-const circle5 = document.getElementById('circle5');
+d3.csv("/../data/mergeddata.csv", function(data) {
+    console.log(data)
+});
+
+const circlenav1 = document.getElementById('circlenav1');
+const circlenav2 = document.getElementById('circlenav2');
+const circlenav3 = document.getElementById('circlenav3');
+const circlelist1 = document.getElementById('circlelist1');
+const circlelist2 = document.getElementById('circlelist2');
 const overlay1 = document.getElementById('overlay1');
 const overlay2 = document.getElementById('overlay2');
 const overlay3 = document.getElementById('overlay3');
 const overlay4 = document.getElementById('overlay4');
-const square1 = document.getElementById('square1');
-const square2 = document.getElementById('square2');
-const triangle1 = document.getElementById('triangle1');
-const triangle2 = document.getElementById('triangle2');
+const squarenav1 = document.getElementById('squarenav1');
+const squarenav2 = document.getElementById('squarenav2');
+const squarenav3 = document.getElementById('squarenav3');
+const squarenav4 = document.getElementById('squarenav4');
+const squareback = document.getElementById('squareback');
+const squarelist1 = document.getElementById('squarelist1');
+const squarelist2 = document.getElementById('squarelist2');
+const trianglelist1 = document.getElementById('trianglelist1');
+const trianglelist2 = document.getElementById('trianglelist2');
+const triangleback = document.getElementById('triangleback');
 const close1 = document.getElementById('close1');
 
 
-circle1.addEventListener('click', () => {
+circlenav1.addEventListener('click', () => {
     overlay1.style.display = 'block';
 });
-circle2.addEventListener('click', () => {
+circlenav2.addEventListener('click', () => {
     overlay1.style.display = 'block';
 });
-circle3.addEventListener('click', () => {
+circlenav3.addEventListener('click', () => {
     overlay1.style.display = 'block';
 });
-circle4.addEventListener('click', () => {
+circlelist1.addEventListener('click', () => {
     overlay3.style.display = 'block';
 });
-circle5.addEventListener('click', () => {
+circlelist2.addEventListener('click', () => {
     overlay3.style.display = 'block';
 });
 
-square1.addEventListener('click', () => {
+squarenav1.addEventListener('click', () => {
     overlay2.style.display = 'block';
     overlay1.style.display = 'none';
 
 });
-square2.addEventListener('click', () => {
+squarenav2.addEventListener('click', () => {
+    overlay2.style.display = 'block';
+    overlay1.style.display = 'none';
+
+});
+squarenav3.addEventListener('click', () => {
+    overlay2.style.display = 'block';
+    overlay1.style.display = 'none';
+
+});
+squarenav4.addEventListener('click', () => {
+    overlay2.style.display = 'block';
+    overlay1.style.display = 'none';
+
+});
+
+squarelist1.addEventListener('click', () => {
+    overlay3.style.display = 'block';
+});
+squarelist2.addEventListener('click', () => {
+    overlay3.style.display = 'block';
+});
+squareback.addEventListener('click', () => {
     overlay1.style.display = 'none';
 });
 
-triangle1.addEventListener('click', () => {
+trianglelist1.addEventListener('click', () => {
     overlay3.style.display = 'block';
-    overlay2.style.display = 'none';
 
 });
-triangle2.addEventListener('click', () => {
+trianglelist2.addEventListener('click', () => {
+    overlay3.style.display = 'block';
+
+});
+triangleback.addEventListener('click', () => {
     overlay2.style.display = 'none';
     overlay1.style.display = 'block';
 });
 
 close1.addEventListener('click', () => {
     overlay3.style.display = 'none';
-    overlay2.style.display = 'block';
 });
-
-
-const circlesContainer = document.getElementById('circles-container');
-const dataArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]; // Your data array here
-
-function createCircles(array) {
-    const numCircles = array.length;
-
-    // Clear existing circles
-    circlesContainer.innerHTML = '';
-
-    for (let i = 0; i < numCircles; i++) {
-        const circle = document.createElement('div');
-        circle.classList.add('circle');
-
-        if (i > 10) {
-            circle.style.marginRight = '10px'; // Add space between two stacks
-        }
-
-        circlesContainer.appendChild(circle);
-    }
-}
-
-createCircles(dataArray);
